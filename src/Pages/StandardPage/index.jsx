0ea9ee4f381c5ee521902e57955279ce60f037b8
Bruno Rivolta <brrivolta@gmail.com>
2023-03-08 15:50:07 -0300

    Criado contexto Cart

--- src/Pages/StandardPage/index.jsx
+++ src/Pages/StandardPage/index.jsx
@@ -1,10 +1,13 @@
+import CartProvider from 'Context/cart'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 export default function StandardPage() {
   return (
     <main>
+      <CartProvider>
         <Outlet />
+      </CartProvider>
     </main>
   )
 }
