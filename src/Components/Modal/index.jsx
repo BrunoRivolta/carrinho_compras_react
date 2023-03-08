fae33605b11939218b4b414516de3ab7831b989c
Bruno Rivolta <brrivolta@gmail.com>
2023-03-08 17:02:01 -0300

    Adicionado db.json

--- src/Components/Modal/index.jsx
+++ src/Components/Modal/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { CartContext } from 'Context/cart'
+import React, { useContext } from 'react'
 import styles from './Modal.module.scss'
 
-export default function Modal({ isOpen, setModal }) {
-    if(isOpen) {
+export default function Modal({ isOpen, setModal }) { 
+
+    const { cart, setCart } = useContext(CartContext)
+
+    function addCart(product) {
+        const newCart = cart
+        if(cart.length === 0) {
+            setCart([product])
+        } else {
+            newCart.forEach((item, index) => {
+                if(product.id === item.id) {
+                    const newProduct = product
+                    const newAmount = product.amount + item.amount
+                    newProduct.amount = newAmount
+                    newCart.splice(index, 1)
+                    newCart.push(newProduct)
+                    setCart([newProduct])
+                } else {
+                    setCart(old => [...old, product])
+                }
+            })
+        }
+
+    }
+
+    const product = {
+        id: 1,
+        title: 'Controle joystick sem fio Sony PlayStation Dualshock 4 green...',
+        img: "https://http2.mlstatic.com/D_NQ_NP_917352-MLA52349227590_112022-O.webp",
+        price: 178,
+        amount: 1
+    }
+
+    if(isOpen) {        
         return (
             <div className={styles.background}>
                 <div className={styles.modal}>
@@ -18,7 +51,7 @@ export default function Modal({ isOpen, setModal }) {
                             <p>frete grátis</p>
                             <p>Quantidade</p>
                             <input type="number" name="" id="" />
-                            <button>Adicionar ao carrinho</button>
+                            <button onClick={() => addCart(product)}>Adicionar ao carrinho</button>
                         </div>
                     </div>
                     <div className={styles.description}>
