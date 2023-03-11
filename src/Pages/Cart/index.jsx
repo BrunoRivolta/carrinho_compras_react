d290a0332b752f0c7d850a76c13588a9e4a48418
BrunoRivolta <brrivolta@gmail.com>
2023-03-11 17:21:22 -0300

    Adiconada pagina de Pedidos

--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -4,10 +4,11 @@ import { OrderContext } from 'Context/order'
 import React, { useContext, useEffect, useState } from 'react'
 import styles from './Cart.module.scss'
 import CartProduct from './CartProduct/CartProduct'
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Cart() {
 
-  const { setOrder } = useContext(OrderContext) 
+  const { order, setOrder } = useContext(OrderContext) 
   const { cart, setCart } = useContext(CartContext)
   const [total, setTotal] = useState(0) 
   const [emptyCart, setEmptyCart] = useState(true)
@@ -28,7 +29,16 @@ export default function Cart() {
   }, [cart])
   
   function payment() {
-    setOrder(cart)
+    const idOrder = uuidv4()
+    const newOrder = [[idOrder, cart]]
+    
+    if(order.length === 0) {
+      setOrder(newOrder)
+    } else {
+      const addOrder = [...order]
+      addOrder.push(newOrder)
+      setOrder(addOrder)
+    }
     setCart([])
     setCompletedPay(true)
   }
@@ -54,7 +64,7 @@ export default function Cart() {
       <div className={styles.empty} style={{ display: alertEmptyState }}>
         <h2>Sem itens no carrinho!</h2>
       </div>
-      <div className={styles.total}>
+      <div className={styles.total} style={{ display: buttonState }}>
         <h2>Total:</h2>
         <p>R$ {total.toFixed(2)}</p>
       </div>
--- src/Pages/Requests/index.jsx
+++ src/Pages/Requests/index.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import styles from './Requests.module.scss'
 
 export default function Requests() {
   return (
-    <div>Requests</div>
+    <section className={styles.container} >
+      <h2 className={styles.title}>Pedidos:</h2>
+      <div className={styles.products}>
+        {cart.map(item => (
+          <CartProduct 
+            key={item.id}
+            img={item.img} 
+            title={item.title} 
+            price={item.price} 
+            qnt={item.qnt}
+          />
+        ))}
+      </div>
+      <div className={styles.empty} style={{ display: alertEmptyState }}>
+        <h2>Sem itens no carrinho!</h2>
+      </div>
+      <div className={styles.total} style={{ display: buttonState }}>
+        <h2>Total:</h2>
+        <p>R$ {total.toFixed(2)}</p>
+      </div>
+      <div className={styles.button} style={{ display: buttonState }} >
+        <Button onClick={() => payment()}>Pagar!</Button>
+      </div>
+      <div className={styles.alert} style={{ display: payState }}>
+        <h2>Pagamento feito com sucesso!</h2>
+        <p>Acesse a pagina de pedido para acompanhar a entrega</p>
+      </div>
+    </section>
   )
 }
