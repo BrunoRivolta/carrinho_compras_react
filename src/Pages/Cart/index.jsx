2ed4cf6f9bbbd03a353145059317846868974c4a
BrunoRivolta <brrivolta@gmail.com>
2023-03-11 16:18:43 -0300

    Criada logica de pagamento

--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -10,19 +10,32 @@ export default function Cart() {
   const { setOrder } = useContext(OrderContext) 
   const { cart, setCart } = useContext(CartContext)
   const [total, setTotal] = useState(0) 
+  const [emptyCart, setEmptyCart] = useState(true)
+  const [completedPlay, setCompletedPay] = useState(false)
   
   useEffect(() => {
     let total = 0
-    cart.forEach(item => {
-      total += item.price
-    })
+
+    if(cart.length === 0) {
+      setEmptyCart(true)
+    } else {
+      cart.forEach(item => {
+        total += item.price
+      })
+      setEmptyCart(false)
+    }
     setTotal(total)
   }, [cart])
   
   function payment() {
     setOrder(cart)
     setCart([])
+    setCompletedPay(true)
   }
+
+  const buttonState = emptyCart === true ? 'none' : 'block' 
+  const payState = completedPlay === false ? 'none' : 'block'
+  const alertEmptyState = emptyCart === false ? 'none' : 'block'  
 
   return (
     <section className={styles.container} >
@@ -38,14 +51,17 @@ export default function Cart() {
           />
         ))}
       </div>
+      <div className={styles.empty} style={{ display: alertEmptyState }}>
+        <h2>Sem itens no carrinho!</h2>
+      </div>
       <div className={styles.total}>
         <h2>Total:</h2>
         <p>R$ {total.toFixed(2)}</p>
       </div>
-      <div className={styles.button}>
+      <div className={styles.button} style={{ display: buttonState }} >
         <Button onClick={() => payment()}>Pagar!</Button>
       </div>
-      <div className={styles.alert}>
+      <div className={styles.alert} style={{ display: payState }}>
         <h2>Pagamento feito com sucesso!</h2>
         <p>Acesse a pagina de pedido para acompanhar a entrega</p>
       </div>
