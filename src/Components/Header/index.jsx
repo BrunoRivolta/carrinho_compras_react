806f14592967d7c27f7870a2e5e996f823a3ac8a
BrunoRivolta <brrivolta@gmail.com>
2023-03-12 11:49:55 -0300

    Refatorando estilos menu e cabecalho

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,33 +3,38 @@ import logo from 'assets/img/code.png'
 import styles from './Header.module.scss'
 import Button from 'Components/Button'
 import { SearchContext } from 'Context/search'
+import banner from 'assets/img/banner.jpeg'
 
 export default function Header() {
 
   const { setSearch } = useContext(SearchContext)
   const [ word, setWord ] = useState()
-  const [ resetValue, setResetValue ] = useState()
 
   const toType = (event) => {
     setWord(event.target.value)
   }
 
   return (
-    <section className={styles.container}>
-      <div className={styles.logo}>
-        <img src={logo} alt="" />
-        <h1>Loja Dev</h1>
-      </div>
-      <div className={styles.search}>
-        <input onChange={toType} type="text" value={resetValue} />
-        <Button 
-          onClick={() => {
-            setSearch(word) 
-            }}
-          >
-            Buscar!
-          </Button>
-      </div>
-    </section>
+    <>
+      <section className={styles.container}>
+        <div className={styles.logo}>
+          <img src={logo} alt="" />
+          <h1>Loja Dev</h1>
+        </div>
+        <div className={styles.search}>
+          <input onChange={toType} type="text" />
+          <Button 
+            onClick={() => {
+              setSearch(word) 
+              }}
+            >
+              Buscar!
+            </Button>
+        </div>
+      </section>
+      <section className={styles.imgContainer}>
+        <img src={banner} alt="" />
+      </section>
+    </>
   )
 }
